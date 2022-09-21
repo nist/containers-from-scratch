2a18d64524417f50e47d8dd35534bee1b8fd22fb
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2022-09-21 18:08:59 -0400

    1. Deconstruct the script

--- main.go
+++ main.go
@@ -25,38 +25,44 @@ func main() {
 func run() {
 	fmt.Printf("Running %v \n", os.Args[2:])
 
+	// cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-		Unshareflags: syscall.CLONE_NEWNS,
+		Cloneflags:   syscall.CLONE_NEWUTS,
+		 // | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	// 	Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	must(cmd.Run())
 }
 
 func child() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Child running %v \n", os.Args[2:])
 
-	cg()
+	syscall.Sethostname([]byte("container"))
+
+	// cg()
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot("/home/liz/ubuntufs"))
-	must(os.Chdir("/"))
-	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-	must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+	// must(syscall.Sethostname([]byte("container")))
+	// must(syscall.Chroot("/home/container"))
+	// must(os.Chdir("/"))
+	// must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	// must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
 
-	must(cmd.Run())
+	cmd.Run()
 
-	must(syscall.Unmount("proc", 0))
-	must(syscall.Unmount("thing", 0))
+	// must(cmd.Run())
+
+	// must(syscall.Unmount("proc", 0))
+	// must(syscall.Unmount("thing", 0))
 }
 
 func cg() {
